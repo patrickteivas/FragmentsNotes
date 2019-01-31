e399923219e4cc02036c293586070d300456950e
patrickteivas <teivas.2011@mail.ru>
2019-01-31 08:44:12 +0200

    Commit

--- App1/MainActivity.cs
+++ App1/MainActivity.cs
@@ -1,23 +1,22 @@
 ﻿using Android.App;
 using Android.OS;
-using Android.Support.V7.App;
-using Android.Runtime;
-using Android.Widget;
-using Android.Views;
 
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Distribute;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace App1
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            AppCenter.Start("b3c9dd2b-93d3-4579-8712-15f4a6ec8e2a", typeof(Distribute));
+            AppCenter.Start("b3c9dd2b-93d3-4579-8712-15f4a6ec8e2a",
+                typeof(Distribute), typeof(Analytics), typeof(Crashes));
 
             SetContentView(Resource.Layout.activity_main);
         }
